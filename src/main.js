dead7d0b6273b6e22ec77c47ba9d003efbcb6921
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-04-13 14:50:21 +0100

    add other functionality to main

--- src/main.js
+++ src/main.js
@@ -2,3 +2,21 @@ import $ from 'jquery';
 import Journal from './journal.js';
 import './css/bootstrap.css';
 import './css/styles.css';
+
+$('#journal-form').submit(function (event) {
+    event.preventDefault();
+    let title = $('#title').val();
+    let body = $('#text-passage').val();
+    let newJournal = new Journal(title, body);
+    $('#wordCounter').text(newJournal.wordCounter());
+    $('#vowelCounter').text(newJournal.charCounter()[0]);
+    $('#consonantCounter').text(newJournal.charCounter()[1]);
+    $('#getTeaser').text(newJournal.getTeaser());
+  
+  
+    $("#fg").click(function () {
+      $("#res").show();
+      $("#results").show();
+    });
+});
+  
