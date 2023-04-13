ef0ac8c375c00ff714224a34c476b935c5254be3
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-04-13 14:38:45 +0100

    add wordcounter prototype added

--- src/journal.js
+++ src/journal.js
@@ -3,3 +3,14 @@ export default function Journal(title, body) {
     this.title = title;
     this.body = body;
 }
+
+// a prototype to count the words in the passage or input. 
+Journal.prototype.wordCounter = function () {
+    if (this.body.trim().length === 0) {
+      return 0;
+    }
+    let totalWords = 0;
+    let wordArray = this.body.split(" ");
+    totalWords = wordArray.length;
+    return totalWords;
+};
